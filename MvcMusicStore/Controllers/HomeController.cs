947bcade197278bf18ad5472745156807705491d
gabriel barrett <gbarrett2@gmail.com>
2017-09-20 22:28:29 -0600

    created album, genre and artist models

--- MvcMusicStore/Controllers/HomeController.cs
+++ MvcMusicStore/Controllers/HomeController.cs
@@ -8,19 +8,25 @@ namespace MvcMusicStore.Controllers
 {
     public class HomeController : Controller
     {
+
+        public ActionResult Message()
+        {
+            ViewBag.Message = "This is a partial view";
+            ViewBag.WatchThis = "this is another line for the partial view to be rendered";
+            return PartialView();
+        }
+
         public ActionResult Index()
         {
             return View();
         }
 
-        public ActionResult About()
+        public ActionResult Edit()
         {
-            ViewBag.Message = "Your application description page.";
-
             return View();
         }
 
-        public ActionResult Contact()
+        public ActionResult Details()
         {
             ViewBag.Message = "Your contact page.";
 
